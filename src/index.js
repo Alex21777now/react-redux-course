065a7ccbc2abd314f2a86da5e98088a78a0191d7
Oleksandr Budnikov <banna707@i.ua>
2022-05-04 20:31:17 +0300

    pre DevTools installing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import {render} from 'react-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux'
+import {compose, createStore} from 'redux'
+import {Provider} from 'react-redux'
+import {rootReducer} from './redux/rootReducer'
 
-const store = createStore()
+const store = createStore(rootReducer, compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+))
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const app = (
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-);
+  </Provider>
+)
+
+render(app, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/redux/postsReducer.js
@@ -0,0 +1,8 @@
+const initialState = {
+   posts: [],
+   FetchedPosts: []
+}
+
+export const postsReducer = (state = initialState, action) => {
+  return state
+}
