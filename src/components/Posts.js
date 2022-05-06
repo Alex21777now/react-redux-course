e71744a060d9f66c644ca200d8d7667dd295f689
Oleksandr Budnikov <banna707@i.ua>
2022-05-06 18:35:02 +0300

    add functionality POSTS now add

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,16 +2,17 @@ import React from 'react'
 import {connect} from 'react-redux'
 import Post from './Post'
 
-const Posts = ({posts}) => {
-  if (!posts.lenght) {
+const Posts = ({syncPosts}) => {
+  if (!syncPosts.length) {
     return <p className="text-center">Постов пока нет</p>
   }
-  return posts.map(post => <Post post={post} key={post} />)
+  return syncPosts.map(post => <Post post={post} key={post.id} />)
 }
 
 const mapStateToProps = state => {
-  console.log(state)
-  return state
+  return {
+    syncPosts: state.posts.posts
+  }
 }
 
 export default connect(mapStateToProps, null)(Posts)
